fbbba88b96f0087a7c0f785ff365a55bfd2fb582
Juan Enrique Segebre Zaghmout <juansegebre@gmail.com>
2016-12-11 03:47:36 -0600

    Ya estan los controllers users, campaigns, comments t donations

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,22 @@
 Rails.application.routes.draw do
+  get 'donations/new'
+
+  get 'comments/new'
+
+  get 'campaigns/new'
+
+  get 'campaigns/index'
+
+  get 'campaigns/show'
+
+  get 'campaigns/edit'
+
+  get 'users/new'
+
+  get 'users/show'
+
+  get 'users/edit'
+
   root 'welcome#index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
